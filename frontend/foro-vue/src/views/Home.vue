cd1d40bfe1e1a5ef80ff19b6a83b5cfc1f482fa2
Gustavo <gus-tavo99@outlook.es>
2021-03-28 22:12:35 -0500

    Modificar HelloWorld

--- frontend/foro-vue/src/views/Home.vue
+++ frontend/foro-vue/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Extraclase de Programación Web"/>
   </div>
 </template>
 
